7462d17d561ae73e015dd65b08e1ff0dbdd8d175
Ravil <megaheend@gmail.com>
2026-02-24 19:51:39 +0400

    Init commit

---
+++ TestGitApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGitApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
